ca3ceee3a7f63b298edf268d431391e044ae75fb
rama11 <prof.agastyo@gmail.com>
2021-11-09 09:43:23 +0700

    add p on index

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 	<h1>This is some testing using heroku</h1>
+	<p>This is test navigation</p>
 	<a href="home.php">Home</a><br>
 	<a href="about.php">About</a><br>
 	<a href="contact_us.php">Contact Us</a><br>
