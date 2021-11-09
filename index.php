52237e27e009474a731417dfa34d93cb6773b4d1
rama11 <prof.agastyo@gmail.com>
2021-11-09 09:31:14 +0700

    new index must be html

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-	echo "hihihi";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Test Index</title>
+</head>
+<body>
+	<h1>This is some testing using heroku</h1>
+	<a href="home.php">Home</a><br>
+	<a href="about.php">About</a><br>
+	<a href="contact_us.php">Contact Us</a><br>
+	<a href="carier.php">Carier</a><br>
+</body>
+</html>
